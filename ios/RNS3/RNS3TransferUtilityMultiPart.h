536220168a145255a399fe342fe1ce8c23c5ef73
Daniel DeKerlegand <dldekerl@gmail.com>
2018-07-18 12:46:10 -0500

    add multipart upload class

---
+++ ios/RNS3/RNS3TransferUtilityMultiPart.h
@@ -0,0 +1,33 @@
+//
+//  RNS3TransferUtilityMultiPart.h
+//  RNS3
+//
+//  Created by Daniel DeKerlegand on 7/18/18.
+//  Copyright © 2018 Facebook. All rights reserved.
+//
+
+// import RCTBridgeModule.h
+#if __has_include(<React/RCTBridgeModule.h>)
+#import <React/RCTBridgeModule.h>
+#else
+#import "RCTBridgeModule.h"
+#endif
+#if __has_include(<React/RCTEventDispatcher.h>)
+#import <React/RCTEventDispatcher.h>
+#else
+#import "RCTEventDispatcher.h"
+#endif
+
+#import <AWSCore/AWSCore.h>
+#import <AWSS3/AWSS3.h>
+
+typedef NS_ENUM(NSInteger, CredentialType) {
+    BASIC,
+    COGNITO
+};
+
+@interface RNS3TransferUtilityMultiPart : NSObject <RCTBridgeModule>
++ (NSMutableDictionary*)nativeCredentialsOptions;
++ (CredentialType)credentialType: (NSString *)type;
++ (void)interceptApplication: (UIApplication *)application handleEventsForBackgroundURLSession:(NSString *)identifier completionHandler:(void (^)())completionHandler;
+@end
